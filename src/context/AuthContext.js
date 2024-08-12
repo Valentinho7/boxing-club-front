94314c843854944dc38d79315ccd2a25412ba8be
valentin <valentin.claquin@gmail.com>
2024-08-12 21:31:00 +0200

    fix

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,26 +1,33 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, { useState, useEffect, createContext } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export const AuthContext = createContext({
   isAuthenticated: false,
-  setIsAuthenticated: () => {},
+  login: () => {},
   logout: () => {},
-  role: '', 
-  setRole: () => {}, 
+  role: '',
+  setRole: () => {},
 });
 
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem('token'));
-  const [role, setRole] = useState(''); // Ajoutez un état pour le rôle
-
+  const [role, setRole] = useState('');
+  const history = useHistory(); // Utiliser useHistory pour la redirection
 
   const logout = () => {
     localStorage.removeItem('token');
     setIsAuthenticated(false);
     setRole('');
+    history.push('/'); // Rediriger vers la page d'accueil
   };
 
   useEffect(() => {
     let timeout;
+
+    function resetTimer() {
+      clearTimeout(timeout);
+      startTimer();
+    }
 
     function startTimer() {
       // Déconnecter l'utilisateur après 10 minutes d'inactivité
@@ -29,26 +36,17 @@ export const AuthProvider = ({ children }) => {
       }, 10 * 60 * 1000);
     }
 
-    function resetTimer() {
-      // Si l'utilisateur fait quelque chose, réinitialiser le délai
-      clearTimeout(timeout);
-      startTimer();
-    }
-
-    // Démarrer le délai lorsque la page est chargée
     startTimer();
 
-    // Réinitialiser le délai chaque fois que l'utilisateur fait quelque chose
-    window.addEventListener('load', resetTimer);
     window.addEventListener('mousemove', resetTimer);
     window.addEventListener('mousedown', resetTimer);
     window.addEventListener('click', resetTimer);
     window.addEventListener('scroll', resetTimer);
     window.addEventListener('keypress', resetTimer);
 
-    // N'oubliez pas de nettoyer l'écouteur d'événements lorsque le composant est démonté
+    // Nettoyer les écouteurs d'événements lorsque le composant est démonté
     return () => {
-      window.removeEventListener('load', resetTimer);
+      clearTimeout(timeout);
       window.removeEventListener('mousemove', resetTimer);
       window.removeEventListener('mousedown', resetTimer);
       window.removeEventListener('click', resetTimer);
@@ -58,7 +56,7 @@ export const AuthProvider = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated, logout,role, setRole, }}>
+    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated, logout, role, setRole }}>
       {children}
     </AuthContext.Provider>
   );
