87ca7b091f5ee52ea7add5011367ed5935ee6e90
valentin <valentin.claquin@gmail.com>
2024-08-12 20:18:07 +0200

    fix: MemberDetails

--- src/controller/MemberDetailsController.js
+++ src/controller/MemberDetailsController.js
@@ -1,46 +1,69 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import UpdateForm from './UpdateMemberController';
+import '../css/style.css';
 
-const DisplaySessionType = () => {
-  const [sessionTypes, setSessionTypes] = useState([]);
+function MemberDetails() {
+  const [memberData, setMemberData] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
 
   useEffect(() => {
     const token = localStorage.getItem('token');
-    if (token) {
-      axios.get('http://34.30.198.59:8081/api/sessions/types', {
-        headers: { Authorization: `Bearer ${token}` },
-      })
-        .then(response => {
-          setSessionTypes(response.data);
-        })
-        .catch(error => {
-          console.error('There was an error fetching the session types!', error);
+  
+    fetch('http://34.30.198.59:8081/api/member/memberDetails', {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${token}`
+      },
+    })
+    .then(response => {
+      const contentType = response.headers.get("content-type");
+      if (contentType && contentType.indexOf("application/json") !== -1) {
+        return response.json();
+      } else {
+        response.text().then(text => {
+          console.error('Server response:', text);
         });
-    } else {
-      console.error('No token found in localStorage');
-    }
+      }
+    })
+    .then(data => {
+      if (data) {
+        setMemberData(data);
+        console.log(data);
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
   }, []);
 
-  if (sessionTypes.length === 0) {
+  const handleUserUpdate = (updatedUser) => {
+    setMemberData(updatedUser);
+    setIsEditing(false);
+  };
+
+  if (!memberData) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div className="container">
-      <h1>Types de Sessions</h1>
-      <div className="row">
-        {sessionTypes.map((type, index) => (
-          <div className="col-md-4" key={index}>
-            <div className="card mb-4">
-              <div className="card-body">
-                <h5 className="card-title">{type.name}</h5>
-              </div>
-            </div>
+    <div className="member-details">
+      <h1>Vos informations</h1>
+      {isEditing ? (
+        <UpdateForm user={memberData} onUserUpdate={handleUserUpdate} />
+      ) : (
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">{memberData.firstname} {memberData.lastname}</h5>
+            <p className="card-text"><strong>Date de naissance:</strong> {memberData.birthdate}</p>
+            <p className="card-text"><strong>Email:</strong> {memberData.email}</p>
+            <p className="card-text"><strong>Numéro de téléphone:</strong> {memberData.phoneNumber}</p>
+            <p className="card-text"><strong>Adresse postale:</strong> {memberData.address}</p>
+            <button className="btn btn-primary" onClick={() => setIsEditing(true)}>Modifier mes informations</button>
           </div>
-        ))}
-      </div>
+        </div>
+      )}
     </div>
   );
-};
+}
 
-export default DisplaySessionType;
+export default MemberDetails;
