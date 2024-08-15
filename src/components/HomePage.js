42a659396fe1c2afde58023c785c74eee823e103
valentin <valentin.claquin@gmail.com>
2024-08-15 14:48:58 +0200

    home

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,7 +2,32 @@ import React from 'react';
 
 function HomePage() {
     return (
-        <div>
+        <div style={{ position: 'relative', marginTop: '60px' }}> {/* Ajuste la marge supérieure si nécessaire pour la navbar */}
+            {/* Image de fond */}
+            <img 
+                src="../img/home-page-boxe.jpg" 
+                alt="Boxing background" 
+                style={{ 
+                    width: '100%', 
+                    height: '100vh', /* Prend toute la hauteur de la fenêtre visible */
+                    objectFit: 'cover' /* Pour s'assurer que l'image ne soit pas déformée */
+                }} 
+            />
+            
+            {/* Texte superposé en haut à gauche */}
+            <div style={{ 
+                position: 'absolute', 
+                top: '20px', 
+                left: '20px', 
+                color: 'white', 
+                fontWeight: 'bold', 
+                textAlign: 'left' 
+            }}>
+                <h1 style={{ fontSize: '4em', margin: 0 }}>La Boxe,<br />à l'état pur.</h1>
+                <p style={{ fontSize: '1.5em', marginTop: '10px' }}>
+                    Cours de boxe, avec ou sans combat.
+                </p>
+            </div>
         </div>
     );
 }
