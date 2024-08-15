5a87074814432fb477a860aa0a69c1b27b23f544
valentin <valentin.claquin@gmail.com>
2024-08-15 15:03:09 +0200

    fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -85,7 +85,7 @@ function Navbar() {
     };
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-white bg-white">
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/">Boxing Club</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
