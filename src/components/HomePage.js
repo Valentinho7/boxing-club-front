d1d9a9e3098bcfd29911488b55c8a9ba744248fa
valentin <valentin.claquin@gmail.com>
2024-08-15 15:27:34 +0200

    mike

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -36,29 +36,29 @@ function HomePage() {
             </div>
 
             {/* New section below the background image */}
-            <div style={{ display: 'flex', alignItems: 'center', padding: '40px', backgroundColor: '#fff' }}>
-                <img 
-                    src={newImage} 
-                    alt="Mike Tyson" 
-                    style={{
-                        width: '180px', /* Adjusted size */
-                        height: 'auto',
-                        borderRadius: '5px', /* Slight border radius */
-                        marginRight: '30px' /* Space between image and text */
-                    }}
-                />
-                <div>
-                    <h2 style={{ margin: '0', fontSize: '1.8em', fontWeight: 'bold', color: '#333' }}>
-                        "Boxing Club permet de rendre la boxe accessible à des débutants"
-                    </h2>
-                    <p style={{ margin: '5px 0', fontSize: '1.2em', fontWeight: 'normal', color: '#555' }}>
-                        Mike Tyson
-                    </p>
-                    <p style={{ fontStyle: 'italic', margin: '0', fontSize: '1em', color: '#777' }}>
-                        Boxeur professionnel
-                    </p>
-                </div>
-            </div>
+            <div style={{ display: 'flex', alignItems: 'center', padding: '40px', backgroundColor: '#fff', border: '1px solid #000' }}>
+    <img 
+        src={newImage} 
+        alt="Mike Tyson" 
+        style={{
+            width: '180px', /* Adjusted size */
+            height: 'auto',
+            borderRadius: '5px', /* Slight border radius */
+            marginRight: '30px' /* Space between image and text */
+        }}
+    />
+    <div>
+        <h2 style={{ margin: '0', fontSize: '1.4em', fontWeight: 'bold', color: '#333' }}>
+            "Boxing Club permet de rendre la boxe accessible à des débutants"
+        </h2>
+        <p style={{ margin: '5px 0', fontSize: '1.2em', fontWeight: 'normal', color: '#555' }}>
+            Mike Tyson
+        </p>
+        <p style={{ fontStyle: 'italic', margin: '0', fontSize: '1em', color: '#777' }}>
+            Boxeur professionnel
+        </p>
+    </div>
+</div>
         </div>
     );
 }
