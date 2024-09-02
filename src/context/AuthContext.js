a232d7eb03921ed092a4b15b08ff0ac23c0d5760
valentin <valentin.claquin@gmail.com>
2024-09-02 16:22:39 +0200

    fix

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -3,6 +3,7 @@ import React, { createContext, useState } from 'react';
 export const AuthContext = createContext({
   isAuthenticated: false,
   setIsAuthenticated: () => {},
+  logout: () => {},
   role: '', 
   setRole: () => {}, 
 });
@@ -11,8 +12,15 @@ export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem('token'));
   const [role, setRole] = useState(''); // Ajoutez un état pour le rôle
 
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    setIsAuthenticated(false);
+    setRole('');
+  };
+
   return (
-    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated, role, setRole }}>
+    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated, logout,role, setRole, }}>
       {children}
     </AuthContext.Provider>
   );
