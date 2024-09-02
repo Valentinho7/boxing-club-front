59a6d8e6b24844c1b9aa4382c66924c091bd1e4b
valentin <valentin.claquin@gmail.com>
2024-09-02 16:21:11 +0200

    fix

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,9 +1,8 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, { createContext, useState } from 'react';
 
 export const AuthContext = createContext({
   isAuthenticated: false,
   setIsAuthenticated: () => {},
-  logout: () => {},
   role: '', 
   setRole: () => {}, 
 });
@@ -12,53 +11,8 @@ export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem('token'));
   const [role, setRole] = useState(''); // Ajoutez un état pour le rôle
 
-
-  const logout = () => {
-    localStorage.removeItem('token');
-    setIsAuthenticated(false);
-    setRole('');
-  };
-
-  useEffect(() => {
-    let timeout;
-
-    function startTimer() {
-      // Déconnecter l'utilisateur après 10 minutes d'inactivité
-      timeout = setTimeout(() => {
-        logout();
-      }, 10 * 60 * 1000);
-    }
-
-    function resetTimer() {
-      // Si l'utilisateur fait quelque chose, réinitialiser le délai
-      clearTimeout(timeout);
-      startTimer();
-    }
-
-    // Démarrer le délai lorsque la page est chargée
-    startTimer();
-
-    // Réinitialiser le délai chaque fois que l'utilisateur fait quelque chose
-    window.addEventListener('load', resetTimer);
-    window.addEventListener('mousemove', resetTimer);
-    window.addEventListener('mousedown', resetTimer);
-    window.addEventListener('click', resetTimer);
-    window.addEventListener('scroll', resetTimer);
-    window.addEventListener('keypress', resetTimer);
-
-    // N'oubliez pas de nettoyer l'écouteur d'événements lorsque le composant est démonté
-    return () => {
-      window.removeEventListener('load', resetTimer);
-      window.removeEventListener('mousemove', resetTimer);
-      window.removeEventListener('mousedown', resetTimer);
-      window.removeEventListener('click', resetTimer);
-      window.removeEventListener('scroll', resetTimer);
-      window.removeEventListener('keypress', resetTimer);
-    };
-  }, []);
-
   return (
-    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated, logout,role, setRole, }}>
+    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated, role, setRole }}>
       {children}
     </AuthContext.Provider>
   );
